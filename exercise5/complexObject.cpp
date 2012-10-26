09b73aaedd849952bc599216a4a61b3f19013ec3
Michael Uman <Michael_Uman@sigmadesigns.com>
2012-10-25 19:52:52 -0700

    Fixed tabs/spaces

--- exercise5/complexObject.cpp
+++ exercise5/complexObject.cpp
@@ -7,58 +7,58 @@
 
 
 /**
- *	A complexObject is a simpleObject with mass & density...
+ *    A complexObject is a simpleObject with mass & density...
  */
  
 complexObject::complexObject(const char* sName, int x, int y, int z, int w, int h, int m, float d)
-:	simpleObject(sName, x, y, z, w, h),
-	m_mass(m),
-	m_density(d)
+:   simpleObject(sName, x, y, z, w, h),
+    m_mass(m),
+    m_density(d)
 {
-	// ctor
+    // ctor
 } 
 complexObject::complexObject(int x, int y, int z, int w, int h, int m, float d)
-:	simpleObject(x, y, z, w, h),
-	m_mass(m),
-	m_density(d)
+:   simpleObject(x, y, z, w, h),
+    m_mass(m),
+    m_density(d)
 {
-	// ctor
+    // ctor
 }
 
 complexObject::~complexObject()
 {
-	// dtor
+    // dtor
 }
 
 /**
- *	Display simpleObjects information along with mass & density
+ *    Display simpleObjects information along with mass & density
  */
  
 void complexObject::display_info(FILE* ofp, const char* sMsg) const 
 {
-	simpleObject::display_info(ofp, sMsg);	// call parent class display_info
-	fprintf(ofp, "Mass = %d lbs Density = %f\n", m_mass, m_density);
+    simpleObject::display_info(ofp, sMsg);    // call parent class display_info
+    fprintf(ofp, "Mass = %d lbs Density = %f\n", m_mass, m_density);
 }
 /**
- *	Display simpleObjects information along with mass & density
+ *    Display simpleObjects information along with mass & density
  */
  
 void complexObject::display_info(FILE* ofp) const 
 {
-	simpleObject::display_info(ofp);	// call parent class display_info
-	fprintf(ofp, "Mass = %d lbs Density = %f\n", m_mass, m_density);
+    simpleObject::display_info(ofp);    // call parent class display_info
+    fprintf(ofp, "Mass = %d lbs Density = %f\n", m_mass, m_density);
 }
 
 /* return the objects mass */
 int complexObject::get_mass() const
 {
-	return m_mass;
+    return m_mass;
 }
 
 /* return the objects density */
 float complexObject::get_density() const 
 {
-	return m_density;
+    return m_density;
 }
 
 
--- exercise5/example5.cpp
+++ exercise5/example5.cpp
@@ -1,13 +1,13 @@
 /**
- *	NAME		:	example1.cpp
- *	PURPOSE		:	Sample C++ project to demonstrate simple class.
- *	DATE		:	October 25, 2012
- *	AUTHOR 		:	Michael Uman
+ *    NAME        :    example1.cpp
+ *    PURPOSE        :    Sample C++ project to demonstrate simple class.
+ *    DATE        :    October 25, 2012
+ *    AUTHOR         :    Michael Uman
  *
- *	NOTE		:	This example shows the use of an externally defined class.
+ *    NOTE        :    This example shows the use of an externally defined class.
  *
- *					Instantiate 2 objects of class simpleObject, use const, ref
- *					passing, and static member function, function overloading,
+ *                    Instantiate 2 objects of class simpleObject, use const, ref
+ *                    passing, and static member function, function overloading,
  *                  sub-classing objects, operator overloading.
  */
  
@@ -17,53 +17,53 @@
 #include "simpleObject.h"
 #include "complexObject.h"
 /**
- *	Main entry point.
+ *    Main entry point.
  */
 
 int user_x_translate = 50;
 
 int main(int argc, const char* argv[])
 {
-	/* Get x translation from commandline */
-	if (argc == 2) {
-		user_x_translate = atoi( argv[1] );
-	}
-	
-	simpleObject	object1("object_1", 100,100,100);
-	simpleObject	object2("object_2", 50, 50, 50, 10, 10);
+    /* Get x translation from commandline */
+    if (argc == 2) {
+        user_x_translate = atoi( argv[1] );
+    }
+    
+    simpleObject    object1("object_1", 100,100,100);
+    simpleObject    object2("object_2", 50, 50, 50, 10, 10);
 
-	/* Use the new function display_info() to output object information */
-	object1.display_info( stdout );
-	object2.display_info( stdout );
+    /* Use the new function display_info() to output object information */
+    object1.display_info( stdout );
+    object2.display_info( stdout );
 
-	printf("\nChecking if objects are equal...\n");
+    printf("\nChecking if objects are equal...\n");
 
-	/* check if these objects occupy the same location */
-	printf("object1 & object2 %s occupy the same place...\n",
-		(object1 == object2)?"DO":"DO NOT");
-		
-	printf("moving object2 by 50,50,50...\n");
-	object2.translate( 50, 50, 50 );
+    /* check if these objects occupy the same location */
+    printf("object1 & object2 %s occupy the same place...\n",
+        (object1 == object2)?"DO":"DO NOT");
+        
+    printf("moving object2 by 50,50,50...\n");
+    object2.translate( 50, 50, 50 );
 
-	object2.display_info( stdout, "object2" );
-	/* check if these objects occupy the same location */
-	printf("object1 & object2 %s occupy the same place...\n",
-		(object1 == object2)?"DO":"DO NOT");
-	
-	printf("\n");
-	
-	/* Test the new complexObject class */
-	complexObject	object3( "object_3", 20, -10, 30, 15, 15, 8, 0.5 );
-	object3.display_info( stdout );
-	
-	/* Attempt to move it out of range!!! */
-	
-	printf("Translating object3 by %d, 0, 0...\n", user_x_translate);
-	if (!object3.translate( user_x_translate, 0, 0 )) {
-		printf("Unable to move object3 by 190, 0, 0!\n");
-	}
-	
-	object3.display_info( stdout );
-	
-	return 0;		
+    object2.display_info( stdout, "object2" );
+    /* check if these objects occupy the same location */
+    printf("object1 & object2 %s occupy the same place...\n",
+        (object1 == object2)?"DO":"DO NOT");
+    
+    printf("\n");
+    
+    /* Test the new complexObject class */
+    complexObject    object3( "object_3", 20, -10, 30, 15, 15, 8, 0.5 );
+    object3.display_info( stdout );
+    
+    /* Attempt to move it out of range!!! */
+    
+    printf("Translating object3 by %d, 0, 0...\n", user_x_translate);
+    if (!object3.translate( user_x_translate, 0, 0 )) {
+        printf("Unable to move object3 by 190, 0, 0!\n");
+    }
+    
+    object3.display_info( stdout );
+    
+    return 0;        
 }
